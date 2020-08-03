93b3a429b9547d8d32074175f807f3b84e2d7e5e
Jujaray Naveen <jujaraynaveen@gmail.com>
2020-08-03 23:25:41 +0530

    Updates

---
+++ precache-manifest.e74ac2c5f2225ea82302a74fd069bc76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e791687a38eb912f7e1cf6f6209785",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "89263501608b9dacb272",
+    "url": "/WeatherApp/static/css/main.69074dc9.chunk.css"
+  },
+  {
+    "revision": "5c8cc8354adf33ac3a73",
+    "url": "/WeatherApp/static/js/2.a96c7494.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/WeatherApp/static/js/2.a96c7494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89263501608b9dacb272",
+    "url": "/WeatherApp/static/js/main.d3a4c727.chunk.js"
+  },
+  {
+    "revision": "f34a18f0592fbbc8ce7e",
+    "url": "/WeatherApp/static/js/runtime-main.dcd8b410.js"
+  },
+  {
+    "revision": "391e023c962de5b3d470638cffbfb82d",
+    "url": "/WeatherApp/static/media/2850815.391e023c.jpg"
+  }
+]);
